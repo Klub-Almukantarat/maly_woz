2ff079b158551ee4aff7c14ff460eb62a6357f50
Mikołaj Nowak <ivan.legnica@gmail.com>
2023-06-18 16:56:22 +0200

    Run on Intel GPU

--- run_image.sh
+++ run_image.sh
@@ -40,20 +40,17 @@ then
     chmod a+r $XAUTH
 fi
 
-DOCKER_OPTS=
+DOCKER_OPTS=""
 
-# Get the current version of docker-ce
-# Strip leading stuff before the version number so it can be compared
-DOCKER_VER=$(dpkg-query -f='${Version}' --show docker-ce | sed 's/[0-9]://')
-if dpkg --compare-versions 19.03 gt "$DOCKER_VER"
+GPU_VENDOR=`lspci | grep ' 3D ' | cut -d" " -f 4`
+if [ -z $GPU_VENDOR ]
 then
-    echo "Docker version is less than 19.03, using nvidia-docker2 runtime"
-    if ! dpkg --list | grep nvidia-docker2
-    then
-        echo "Please either update docker-ce to a version greater than 19.03 or install nvidia-docker2"
-	exit 1
-    fi
-    DOCKER_OPTS="$DOCKER_OPTS --runtime=nvidia"
+    GPU_VENDOR=`lspci | grep ' VGA ' | cut -d" " -f 5`
+fi
+
+if [ $GPU_VENDOR = "Intel" ]
+then
+    DOCKER_OPTS="$DOCKER_OPTS -v \"/dev/dri:/dev/dri\" --device /dev/dri"
 else
     DOCKER_OPTS="$DOCKER_OPTS --gpus all"
 fi
