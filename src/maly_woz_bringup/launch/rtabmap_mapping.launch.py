41b6422da7a58ffd19515a13b9f943f32539f577
Michal Wojcik <wojcikmichal98@gmail.com>
2023-08-03 02:04:18 +0200

    Add some RTAB-Map launch

---
+++ src/maly_woz_bringup/launch/rtabmap_mapping.launch.py
@@ -0,0 +1,29 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+    pkg_rtabmap = get_package_share_directory("rtabmap_launch")
+
+    rtabmap_mapping = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_rtabmap, "launch", "rtabmap.launch.py")
+        ),
+        launch_arguments={
+            "rtabmap_args": "--delete_db_on_start",
+            "depth_topic": "/realsense_d435/depth_image",
+            "rgb_topic": "/realsense_d435/image",
+            "camera_info_topic": "/realsense_d435/camera_info",
+            "approx_sync": "false",
+        }.items(),
+    )
+
+    return LaunchDescription(
+        [
+            rtabmap_mapping,
+        ]
+    )
