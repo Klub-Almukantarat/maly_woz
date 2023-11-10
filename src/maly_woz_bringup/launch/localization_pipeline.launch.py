5cec2ff11a6c6630e2d13b439b7085be44053998
Michał Wójcik <wojcikmichal98@gmail.com>
2023-11-11 00:46:51 +0100

    Configs for global localization

---
+++ src/maly_woz_bringup/launch/localization_pipeline.launch.py
@@ -0,0 +1,38 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    mikf_filter = Node(
+        package="localization_dtu",
+        executable="mikf_node",
+        remappings=[
+            ("wheel_odom", "/model/rover/odometry"),
+            ("vo_odom", "/rtabmap/odom"),
+            ("output", "/mikf_odom"),
+        ],
+    )
+
+    global_localization = Node(
+        package="localization_dtu",
+        executable="global_localization",
+        remappings=[
+            ("odom_in", "/mikf_odom"),
+            ("points", "/realsense_d435/points"),
+            ("odom_out", "/global_odom"),
+        ],
+        parameters=[
+            {
+                "map_path": "/home/developer/rover_ws/data/warehouse.ply",
+                "visualize": True,
+                "use_sim_time": True,
+            }
+        ],
+    )
+
+    return LaunchDescription(
+        [
+            mikf_filter,
+            global_localization,
+        ]
+    )
